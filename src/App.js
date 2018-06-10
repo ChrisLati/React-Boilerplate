f65376437e0d994be1f392f5cbcc2ae123f6b6c5
ChrisLati <christopher@winks-designs.com>
2018-06-10 13:16:38 -0400

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  async componentDidMount () {
+  try {
+    const response =  await  axios.get('/healthcheck')
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        Hellow World!
+      </div>
+    );
+  }
+}
+
+export default App;
